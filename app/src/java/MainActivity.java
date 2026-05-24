280c80b3957070a04f5e5efd3b5c486a372f652a
Ismael Ch <ismael.chavez0406@gmail.com>
2026-05-24 12:25:02 -0600

    proyect created and ended video #9

---
+++ app/src/java/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.ernesto.miprimerapp;
+
+import android.os.Bundle;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        int matematicas = 5;
+        int quimica = 5;
+        int fisica = 5;
+        int promedio = 0;
+
+        promedio = (matematicas + quimica + fisica) / 3;
+
+        if (promedio >= 6) {
+            Toast.makeText(this, "Aprobado", Toast.LENGTH_SHORT).show();
+        } else if (promedio <= 5) {
+            Toast.makeText(this, "Reprobado", Toast.LENGTH_LONG).show();
+        }
+    }
+}
