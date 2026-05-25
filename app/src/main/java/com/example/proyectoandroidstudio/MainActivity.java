b3f876a8940a96a3b6b2602e7dd32bacd7263d02
Hector <hectorivangonzaleza1234@gmail.com>
2026-05-25 13:25:09 -0600

    Final

--- app/src/main/java/com/example/proyectoandroidstudio/MainActivity.java
+++ app/src/main/java/com/example/proyectoandroidstudio/MainActivity.java
@@ -1,17 +1,22 @@
 package com.example.proyectoandroidstudio;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
+import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText et1;
     private EditText et2;
     private TextView tv1;
+
+    private DecimalFormat formato = new DecimalFormat("#.##");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,8 +28,14 @@ public class MainActivity extends AppCompatActivity {
         tv1 = findViewById(R.id.txt_resultado);
     }
 
-    // Un método auxiliar para verificar si los campos están vacíos y ahorrar código
-    private boolean validarCampos() {
+    private void ocultarTeclado(View view) {
+        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        if (imm != null) {
+            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
+    }
+
+    private boolean validar() {
         String valor1 = et1.getText().toString().trim();
         String valor2 = et2.getText().toString().trim();
 
@@ -35,47 +46,56 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
-    // --- MÉTODOS DE OPERACIÓN ---
+    public void Limpiar(View view) {
+        et1.setText("");
+        et2.setText("");
+        tv1.setText("0");
+        et1.requestFocus();
+        ocultarTeclado(view);
+    }
 
     public void Sumar(View view) {
-        if (validarCampos()) {
+        ocultarTeclado(view);
+        if (validar()) {
             double num1 = Double.parseDouble(et1.getText().toString());
             double num2 = Double.parseDouble(et2.getText().toString());
             double suma = num1 + num2;
-            tv1.setText(String.valueOf(suma));
+            tv1.setText(formato.format(suma));
         }
     }
 
     public void Restar(View view) {
-        if (validarCampos()) {
+        ocultarTeclado(view);
+        if (validar()) {
             double num1 = Double.parseDouble(et1.getText().toString());
             double num2 = Double.parseDouble(et2.getText().toString());
             double resta = num1 - num2;
-            tv1.setText(String.valueOf(resta));
+            tv1.setText(formato.format(resta));
         }
     }
 
     public void Multiplicar(View view) {
-        if (validarCampos()) {
+        ocultarTeclado(view);
+        if (validar()) {
             double num1 = Double.parseDouble(et1.getText().toString());
             double num2 = Double.parseDouble(et2.getText().toString());
-            double multiplicacion = num1 * num2;
-            tv1.setText(String.valueOf(multiplicacion));
+            double mult = num1 * num2;
+            tv1.setText(formato.format(mult));
         }
     }
 
     public void Dividir(View view) {
-        if (validarCampos()) {
+        ocultarTeclado(view);
+        if (validar()) {
             double num1 = Double.parseDouble(et1.getText().toString());
             double num2 = Double.parseDouble(et2.getText().toString());
 
-            // Validación crucial: No se puede dividir entre cero
             if (num2 == 0) {
-                Toast.makeText(this, "El universo explotará si divides entre cero", Toast.LENGTH_LONG).show();
+                Toast.makeText(this, "No se puede dividir entre cero", Toast.LENGTH_SHORT).show();
                 tv1.setText("Error");
             } else {
-                double division = num1 / num2;
-                tv1.setText(String.valueOf(division));
+                double div = num1 / num2;
+                tv1.setText(formato.format(div));
             }
         }
     }
